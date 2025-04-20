d9dffd4fc788dfb8697664e9c40f59d781e5a149
Nalin Parihar <nalin7parihar@gmail.com>
2025-04-21 00:09:33 +0530

    added transaction summary

--- backend/controllers/transaction.controller.js
+++ backend/controllers/transaction.controller.js
@@ -27,4 +27,54 @@ const addTransaction = async (req,res) => {
     
   }
 }
-export {getUserTransactions,addTransaction};
+
+const updateTransaction = async (req,res) => {
+  const {id} = req.params;
+  const {amount,description,category,senderId,receiverId} = req.body;
+  try {
+    if(!amount || !description || !category || !senderId || !receiverId) {
+      return res.status(400).json({message : "Please fill all the fields"});
+    }
+    const transaction = await Transactions.findByIdAndUpdate(id,{amount,description,category,senderId,receiverId},{new : true});
+    if(!transaction) {
+      return res.status(404).json({message : "Transaction not found"});
+    }
+    transaction.save();
+    return res.status(200).json({message : "Transaction updated Successfully",transaction});
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const getTransactionSummary = async (req,res) => {
+  const {id} = req.user;
+
+  const transactions = await Transactions.find({$or  : [{senderId:  id,receiverId : id}]}).populate("senderId receiverId","name");
+  let totalYouOwe = 0;
+  let totalYouAreOwed = 0;
+  const friendBalances = {};
+
+  transactions.forEach(txn => {
+    const isSender = txn.senderId._id.equals(id);
+    const otherUser = isSender ? txn.receiverId : txn.senderId;
+
+    if(!friendBalances[otherUser._id]) {
+      friendBalances[otherUser._id] = {
+        name : otherUser.name,
+        balance : 0
+      }
+    }
+    const amount = txn.amount;
+    if(isSender) {
+      totalYouOwe += amount;
+      friendBalances[otherUser._id].balance -= amount;
+    } else {
+      totalYouAreOwed += amount;
+      friendBalances[otherUser._id].balance += amount;
+    }
+  })
+    const netBalance = totalYouAreOwed - totalYouOwe;
+
+    res.json({totalYouAreOwed,totalYouOwe,netBalance,friendBalances});
+}
+export {getUserTransactions,addTransaction,updateTransaction,getTransactionSummary};
--- backend/routes/transaction.route.js
+++ backend/routes/transaction.route.js
@@ -1,8 +1,9 @@
 import express from "express";
 import verifyToken from "../middleware/auth.js";
+import { getUserTransactions,addTransaction,updateTransaction } from "../controllers/transaction.controller.js";
 const transactionRouter = express.Router();
 transactionRouter.get("/getTransactions",verifyToken,getUserTransactions);
 transactionRouter.post("/",verifyToken,addTransaction);
-transactionRouter.get("/:id", verifyToken, getTransactionById);
 transactionRouter.put("/:id", verifyToken, updateTransaction);
+transactionRouter.get("/getSummary",verifyToken,getTransactionSummary);
 export default transactionRouter;
