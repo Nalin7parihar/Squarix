3e1c9107d8b763243cab61e501635dbceebe8dda
Nalin Parihar <105600321+Nalin7parihar@users.noreply.github.com>
2025-05-12 22:54:59 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 app.use(cookieParser());
 app.use(cors(
- { origin: 'http://localhost:3000', // frontend origin
+ { origin: 'http://localhost:3000', 'https://squarix.vercel.app/' // frontend origin
   credentials: true} 
 ));
 
