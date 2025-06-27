b9953706d18ca5325ec8ea3f32f7dea10dd12cfb
Nalin Parihar <105600321+Nalin7parihar@users.noreply.github.com>
2025-06-27 23:29:55 +0530

    Update server.js for deployment

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:3000', // Add your new Vercel subdomain here
+  origin: ['http://localhost:3000','https://squarix.vercel.app/'], // Add your new Vercel subdomain here
   credentials: true,  // If you need to send cookies or headers along with the request
 }));
 
